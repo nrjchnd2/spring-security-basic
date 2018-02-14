1f5f79326e8e18b562b3e46882bc79b9f0c5434d
neera <neeraj_chand@outlook.com>
2018-02-15 00:14:55 +0530

    role based authorization

--- src/main/java/com/neeraj/springsecurity/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/neeraj/springsecurity/springsecurity/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.neeraj.springsecurity.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
 
@@ -15,12 +17,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("neeraj").password("naxi").roles("USER").and()
-			.withUser("suman").password("suman").roles("USER");
+			.withUser("suman").password("suman").roles("ADMIN");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			
+			.antMatchers("/security/rest").hasRole("ADMIN")
 			.anyRequest()
 			.fullyAuthenticated()
 			.and().httpBasic();
